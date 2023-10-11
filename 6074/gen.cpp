3675d65cec238ad89ed5fc5f4a632a9aec019cc2
ZHOUYI-UM <91511540+ZHOUYI-UM@users.noreply.github.com>
2023-10-11 13:59:17 +0800

    6074 AC代码

---
+++ 6074/gen.cpp
@@ -0,0 +1,48 @@
+//http://47.110.135.197/problem.php?id=6074
+//6074: DP25 删除相邻数字的最大分数
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef pair<LL,LL> PLL;
+
+const int N=2e6+10;
+
+string s="abcdefghijklmnopqrstuvwxyz";
+
+LL ran(LL l=1,LL r=1e3) {
+	if (l>r) {
+		swap(l,r);
+	}
+	return rand()%(r-l+1)+l;
+}
+
+int main(int argc, char *argv[]) {
+	int seed = time(NULL);
+	if (argc>1) {
+		//有参数
+		seed = atoi(argv[1]);
+	}
+	srand(seed);
+	
+	freopen("09.in", "w", stdout);
+	
+	LL T = 1;
+	//cout<<T<<"\n";
+	
+	while (T--)	{
+		//n
+		LL n = ran(1e5,1e5);
+		//LL m = ran(1,n);
+		cout<<n<<"\n";
+		//ai
+		for(LL i=1;i<=n;i++){
+			//模式
+			LL x = ran(1,1e4);
+			cout<<x<<" ";		
+		}
+		cout<<"\n";
+	}
+	
+	return 0;
+}
