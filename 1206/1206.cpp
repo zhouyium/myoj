2b2c3f34305e2262ae98cf07db539835b6efcd88
zhouyi <70430188+zhouyium@users.noreply.github.com>
2023-10-16 09:48:54 +0800

    "整理药名" AC代码

---
+++ 1206/1206.cpp
@@ -0,0 +1,66 @@
+//1206: §1 5 整理药名
+//http://47.110.135.197/problem.php?id=1206
+
+#include<bits/stdc++.h>
+using namespace std;
+using LL=long long;
+using PLL=pair<LL,LL>;
+const LL INF=0x3f3f3f3f3f3f3f3f;
+const double EPS=1e-10;
+const LL MO=1e9+7;
+
+const int N=5e5+10;
+
+void solve() {
+	LL n;
+	cin>>n;
+	for(LL i=1;i<=n;i++){
+		string s;
+		cin>>s;
+		LL m=s.size();
+		s[0]=toupper(s[0]);//第一个字符变大写
+		for(LL j=1;j<m;j++){
+			s[j]=tolower(s[j]);
+		}
+		cout<<s<<"\n";
+	}
+}
+
+int main() {
+#if 0
+	//提交到OJ
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	LL T=1;
+	//cin>>T;
+	while(T--) {
+		//cout<<i<<"\n";
+		solve();
+	}
+#else
+	int n=10;
+	for(int i=0;i<n;i++){
+		char in[16];
+		sprintf(in, "%02d.in", i);
+		char out[16];
+		sprintf(out, "%02d.out", i);
+		
+		freopen(in, "r", stdin);
+		freopen(out, "w", stdout);
+		
+		LL T=1;
+		//cin>>T;
+		while(T--) {
+			//cout<<i<<"\n";
+			solve();
+		}
+		
+		fclose(stdin);
+		fclose(stdout);
+	}
+#endif
+	
+	return 0;
+}
---
+++ 1206/gen.cpp
@@ -0,0 +1,50 @@
+//1206: §1 5 整理药名
+//http://47.110.135.197/problem.php?id=1206
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef pair<LL,LL> PLL;
+
+const int N=2e6+10;
+
+string s="0123456789-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+LL ran(LL l=1,LL r=1e3) {
+	if (l>r) {
+		swap(l,r);
+	}
+	return rand()%(r-l+1)+l;
+}
+
+int main(int argc, char *argv[]) {
+	int seed = time(NULL);
+	if (argc>1) {
+		//有参数
+		seed = atoi(argv[1]);
+	}
+	srand(seed);
+	
+	freopen("09.in", "w", stdout);
+	
+	LL T = 1;
+	//cout<<T<<"\n";
+	
+	while (T--)	{
+		//n
+		LL n = ran(1e2,1e2);
+		cout<<n<<"\n";
+		for(LL i=1;i<=n;i++){
+			//药名长度
+			LL m = ran(1,20);
+			for(LL j=1;j<=m;j++){
+				LL x = ran(0, s.size()-1);
+				cout<<s[x];
+			}
+			cout<<"\n";
+		}
+		cout<<"\n";
+	}
+	
+	return 0;
+}
